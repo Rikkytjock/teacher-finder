b1dfba5f3e23288555446261cc991d5cf216acbd
Rikard <mohlin.rikard@gmail.com>
2024-07-30 12:27:41 +0200

    added assets folder and logo image

--- apps/teacher-finder-frontend/src/app/app.component.ts
+++ apps/teacher-finder-frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { FooterComponent } from './components/footer/footer.component'
+import { HeaderComponent } from './components/header/header.component'
 import { RegisterComponent } from './components/register/register.component'
 
 @Component({
   standalone: true,
-  imports: [RouterModule, RegisterComponent],
+  imports: [RouterModule, RegisterComponent, HeaderComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
