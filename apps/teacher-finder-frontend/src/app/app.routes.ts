fe04d76605a5f8a492e2b143c7fcc88048b785d3
Rikard <mohlin.rikard@gmail.com>
2024-08-01 13:19:41 +0200

    added routing and routerlinks

--- apps/teacher-finder-frontend/src/app/app.routes.ts
+++ apps/teacher-finder-frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Route } from '@angular/router'
+import { LoginPageComponent } from './pages/login-page/login-page.component'
 
-export const appRoutes: Route[] = []
+export const appRoutes: Route[] = [{ path: 'login', component: LoginPageComponent }]
