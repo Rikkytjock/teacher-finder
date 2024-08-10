471140b836a3cfd08c3d9ae525c49444d9defe44
Rikard <mohlin.rikard@gmail.com>
2024-08-10 10:33:34 +0200

    made pages for my programs and add programs

---
+++ apps/teacher-finder-frontend/src/app/pages/add-program-page/add-program-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-add-program-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-program-page.component.html',
+  styleUrl: './add-program-page.component.scss'
+})
+export class AddProgramPageComponent {}
---
+++ apps/teacher-finder-frontend/src/app/pages/my-programs-page/my-programs-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-my-programs-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './my-programs-page.component.html',
+  styleUrl: './my-programs-page.component.scss'
+})
+export class MyProgramsPageComponent {}
