afec083e24a98635befb60386a7f6c76f6efe1c4
Rikard <mohlin.rikard@gmail.com>
2024-08-26 12:13:54 +0200

    created function to submit function and add ng form it is NOT working at the moment

--- apps/teacher-finder-frontend/src/app/pages/form-test-page/form-test-page.component.ts
+++ apps/teacher-finder-frontend/src/app/pages/form-test-page/form-test-page.component.ts
@@ -1,12 +1,32 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-form-test-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './form-test-page.component.html',
   styleUrl: './form-test-page.component.scss'
 })
-export class FormTestPageComponent {}
+export class FormTestPageComponent {
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
+
+  submitApplication() {
+    this.applyForm.submitApplication(
+      // denna är fel
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email)
+  }
+}
