46a2170b903cd42e77c97550ba0069fdd9cba0d0
Rikard <mohlin.rikard@gmail.com>
2024-08-26 11:41:07 +0200

    created new page to test ng forms

---
+++ apps/teacher-finder-frontend/src/app/pages/form-test-page/form-test-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-form-test-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-test-page.component.html',
+  styleUrl: './form-test-page.component.scss'
+})
+export class FormTestPageComponent {}
